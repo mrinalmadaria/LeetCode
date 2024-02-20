91784e1b67abe00a89aa8f7b49fc2781bfe33650
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-02-20 13:13:03 +0530

    Feb 20 POTD

---
+++ POTD/problem_268.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v(n+1, -1);
+        for(int i = 0; i < n; i++){
+            v[nums[i]] = nums[i];
+        }
+        for(int i = 0; i < v.size(); i++){
+            if(v[i] == -1) return i;
+        }
+        return 0;
+    }
+};
