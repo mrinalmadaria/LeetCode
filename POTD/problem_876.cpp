e38f35fc6c9f9e46345644554d730182207a4645
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-03-07 21:57:35 +0530

    March 7 POTD

---
+++ POTD/problem_876.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+int val;
+ListNode *next;
+ListNode() : val(0), next(nullptr) {}
+ListNode(int x) : val(x), next(nullptr) {}
+ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slowPointer = head;
+        ListNode *fastPointer = head;
+
+        while(fastPointer != nullptr && fastPointer -> next != nullptr){
+            slowPointer = slowPointer -> next;
+            fastPointer = fastPointer -> next -> next;
+        }
+        return slowPointer;
+    }
+};
