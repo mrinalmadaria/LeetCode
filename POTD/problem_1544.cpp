1e6c2e75d6ce52c6e253870bbcb7d507d1fb5fb2
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-04-05 19:29:02 +0530

    April 5 POTD

---
+++ POTD/problem_1544.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty()) st.push(s[i]);
+            else if(abs(st.top()-s[i])==32)st.pop();
+            else st.push(s[i]);
+        }
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
