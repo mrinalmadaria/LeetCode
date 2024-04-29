02718e2943a66b43196be33b7e357296ae378b8f
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-04-29 21:31:01 +0530

    April 29 POTD

---
+++ POTD/problem_2997.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        for (auto& x : nums) {
+            ans = ans ^ x;
+        }
+        ans = ans ^ k;
+        int res = 0;
+        while (ans > 0) {
+            if (ans & 1)
+                res++;
+            ans = ans >> 1;
+        }
+        return res;
+    }
+};
