6efe5f6507433ea4517e0f44133b956b03a24013
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-05-01 21:28:57 +0530

    May 1 POTD

---
+++ POTD/problem_2000.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int j = word.find(ch);
+        if(j != -1){
+            reverse(word.begin(), word.begin() + j + 1);
+        }
+        return word;
+    }
+};
