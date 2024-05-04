595673703b727f6f351ebbbdd10a19d98743cb59
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-05-04 18:44:21 +0530

    May 4 POTD

---
+++ POTD/problem_881.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin() , people.end());
+
+        int i=0 , boats=0;
+        int j=people.size()-1;
+
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+                boats++;
+            }
+            else{
+                boats++;
+                j--;
+            }
+        }
+
+        return boats;
+    }
+};
