b278a5cdecb43292288331b0376e7a864889c5ce
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-05-30 22:53:05 +0530

    May 30 POTD

---
+++ POTD/problem_1442.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] ^ arr[i];
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int k = i + 1; k < n; ++k) {
+                if (prefix[i] == prefix[k + 1]) {
+                    count += (k - i);
+                }
+            }
+        }
+
+        return count;
+    }
+};
