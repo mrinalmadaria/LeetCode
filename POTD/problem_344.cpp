f695d9e52e84cc4ff1e52feb655aa1723c414347
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-06-02 22:38:35 +0530

    June 2 POTD

---
+++ POTD/problem_344.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++; right--;
+        }
+    }
+};
