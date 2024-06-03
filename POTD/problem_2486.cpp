096cb2b030f58645708c47cf65de83c91140816f
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-06-03 23:21:48 +0530

    June 3 POTD

---
+++ POTD/problem_2486.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int s_index = 0, t_index = 0;
+        int s_length = s.length(), t_length = t.length();
+    
+        while (s_index < s_length && t_index < t_length) {
+            if (s[s_index] == t[t_index]) {
+                t_index++;
+            }
+            s_index++;
+        }
+    
+        return t_length - t_index;
+    }
+};
