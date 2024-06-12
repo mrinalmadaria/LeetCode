832b5a336e29512bb7a21dae26e3b8afb2eced26
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-06-12 21:48:13 +0530

    June 12 POTD

---
+++ POTD/problem_75.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, one = 0, two = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0) zero++;
+            else if(nums[i] == 1) one++;
+            else two++;
+        }
+
+        for(int i = 0; i < zero; i++){
+            nums[i] = 0;
+        }
+        for(int i = zero; i < zero + one; i++){
+            nums[i] = 1;
+        }
+        for(int i = zero + one; i < nums.size(); i++){
+            nums[i] = 2;
+        }
+    }
+};
