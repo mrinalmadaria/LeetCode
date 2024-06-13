bcfbaed2cf12252a6052c6fd3eaf246762b8c86d
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-06-13 22:40:44 +0530

    June 13 POTD

---
+++ POTD/problem_2037.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int moves = 0;
+        for( int i=0; i<seats.size(); i++)
+            moves += abs(seats[i] - students[i]);
+        
+        return moves;
+    }
+};
