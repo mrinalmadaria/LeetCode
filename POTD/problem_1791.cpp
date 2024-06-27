c5c2e66e2f616dd5ed4fd56602da8e33aee9b04c
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-06-27 23:24:06 +0530

    June 27 POTD

---
+++ POTD/problem_1791.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& e) {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+    }
+};
