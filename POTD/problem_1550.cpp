a7720e5155155fd0ec4795c9a52f0d735c7c80f1
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-07-01 23:14:39 +0530

    July 1 POTD

---
+++ POTD/problem_1550.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+
+        for (int num : arr){
+            if (num % 2 != 0){
+                count++;
+                if (count == 3){
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+};
