4a8d453b9c4f7789ed57052f65b3a490b65358ac
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-07-10 18:43:18 +0530

    July 10 POTD

---
+++ POTD/problem_1598.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        vector<string> paths_stack;
+
+        for (const string& log : logs) {
+            if (log == "../") {
+                if (!paths_stack.empty()) {
+                    paths_stack.pop_back();
+                }
+            } else if (log != "./") {
+                paths_stack.push_back(log);
+            }
+        }
+        return paths_stack.size();
+    }
+};
