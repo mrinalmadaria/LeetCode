524d95e4e144f33e29c7630a12a5a7324758b44f
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-07-11 21:01:38 +0530

    July 11 POTD

---
+++ POTD/problem_1190.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        deque<int> ind_stack;
+        vector<char> res;
+
+        for (char char_s : s) {
+            if (char_s == '(') {
+                ind_stack.push_back(res.size());
+            } else if (char_s == ')') {
+                int start_ind = ind_stack.back();
+                ind_stack.pop_back();
+                reverse(res.begin() + start_ind, res.end());
+            } else {
+                res.push_back(char_s);
+            }
+        }
+
+        return string(res.begin(), res.end());
+    }
+};
