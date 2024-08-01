ec9ce5aa377321d3b62e71f7776fff9485e26f5e
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-08-01 23:16:00 +0530

    August 1 POTD

---
+++ POTD/problem_2678.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for(auto &x : details){
+            int age = stoi(x.substr(11,2));
+            ans += age > 60;
+        }
+        return ans;
+    }
+};
