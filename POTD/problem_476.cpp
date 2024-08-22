56d4bea543f848daf5e6e3e7ec747762a68eafbf
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-08-22 23:40:55 +0530

    August 22 POTD

---
+++ POTD/problem_476.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc.h>
+using namespace std;
+
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
+        }
+        
+        return ~mask & ~num;
+    }
+};
