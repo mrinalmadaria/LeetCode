117b56001b132500701a30b7075c40ec562b0d34
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-08-26 16:24:10 +0530

    August 26 POTD

---
+++ POTD/problem_590.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorder(Node *root){
+        if(!root) return ans;
+        for(auto &child : root->children){
+            postorder(child);
+        }
+        ans.push_back(root->val);
+        return ans;
+    }
+};
