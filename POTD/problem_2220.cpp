75eb96bc00fb2258e6372b684c25028425ef6fde
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-09-11 23:52:04 +0530

    September 11 POTD

---
+++ POTD/problem_2220.cpp
@@ -0,0 +1,16 @@
+#include<bits/sdtc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorResult = start ^ goal;
+        int ans = 0;
+
+        while(xorResult > 0){
+            ans += xorResult & 1;
+            xorResult >>= 1;
+        }
+        return ans;
+    }
+};
