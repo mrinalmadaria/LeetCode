85ceaf7ec33c951359afcb8691715325e037ffaf
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-10-09 23:19:13 +0530

    October 9 POTD

---
+++ POTD/problem_921.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> S;
+        int c = 0;
+
+        for(char i : s){
+            if(i == '('){
+                S.push(i);
+            }else{
+                if(S.empty()){
+                    c++;
+                }else{
+                    S.pop();
+                }
+            }
+        }
+        return c + S.size();
+    }
+};
