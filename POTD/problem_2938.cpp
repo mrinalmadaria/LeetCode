fa6097cbc9aff3cebfb7e775a7927863540fe52f
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-10-15 23:41:29 +0530

    October 15 POTD

---
+++ POTD/problem_2938.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') 
+                swap += (long long) black; 
+            else
+                black++; 
+        }
+        return swap;
+    }
+};
