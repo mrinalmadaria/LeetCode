4fdf4e6a5db8c3acae7ace738fb5cb953872412b
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-11-26 23:33:05 +0530

    November 26 POTD

---
+++ POTD/problem_2924.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    static int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> deg(n, 0);
+        for(auto& e: edges){
+            int w=e[1];
+            deg[w]++;
+        }
+        vector<int> deg0;
+        for(int i=0; i<n; i++){
+            if (deg[i]==0)
+                deg0.push_back(i);
+        }
+        if (deg0.size()!=1) return -1;
+        else return deg0[0];
+    }
+};
