923c16effdb6d73a4064b175b8c187293a234b0c
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-12-16 23:14:19 +0530

    December 16 POTD

---
+++ POTD/problem_3264.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    using int2=pair<int, int>;
+    static vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        const int n=nums.size();
+        vector<int2> heap(n);
+        for(int i=0; i<n; i++){
+            heap[i]={nums[i], i};
+        }
+        make_heap(heap.begin(), heap.end(), greater<>{});
+        for(int i=0; i<k; i++){
+            pop_heap(heap.begin(), heap.end(), greater<>{});
+            heap.back().first*=multiplier;
+            push_heap(heap.begin(), heap.end(), greater<>{});
+        }
+        for(auto& [x, i]: heap)
+            nums[i]=x;
+
+        return nums;
+    }
+};
