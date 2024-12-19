222f71fbb40794b1f6699f405407e9fb9e830b47
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-12-19 17:06:58 +0530

    December 19 POTD

---
+++ POTD/problem_769.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        const int n=arr.size();
+        int cnt=0, diff=0;
+        for(int i=0; i<n; i++){
+            diff+=arr[i]-i;
+            cnt+=(diff==0);
+        }
+        return cnt;
+    }
+};
