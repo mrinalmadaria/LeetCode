9fec867c63edee28e89f1214e5c25ce7e1b14623
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-01-12 23:47:55 +0530

    January 12 POTD

---
+++ POTD/problem_2116.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    static bool canBeValid(string& s, string& locked) {
+        const int n=s.size();
+        if (n&1) return 0;
+        int bMin=0, bMax=0;
+        for(int i=0; i<n; i++){
+            bool op=s[i]=='(', wild=locked[i]=='0';
+            bMin+=(!op|wild)?-1:1;
+            bMax+=(op|wild)?1:-1;
+            if (bMax<0) return 0;
+            bMin=max(bMin, 0);
+        }
+        return bMin==0;
+    }
+};
