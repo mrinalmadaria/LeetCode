ed4297c205d78122958d5c12336e876c70bc47e7
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-02-02 23:08:14 +0530

    February 2 POTD

---
+++ POTD/problem_1752.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0, n = nums.size();
+        for (int i = 0; i < n; i++) 
+            if (nums[i] > nums[(i+1)%n] && ++count > 1) return false;
+        return true;
+    }
+};
