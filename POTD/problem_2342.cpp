adbd2a75cbc61701a6c6cbaec3f955aa79b02145
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-02-12 21:36:49 +0530

    February 12 POTD

---
+++ POTD/problem_2342.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int maximumSum(vector<int>& nums) {
+            int mp[82];  // Stores max num for each digit sum
+            memset(mp, -1, sizeof(mp));
+            int ans = -1;
+    
+            for (int num : nums) {
+                int sumDigits = 0, temp = num;
+                while (temp) sumDigits += temp % 10, temp /= 10;
+    
+                if (mp[sumDigits] != -1)
+                    ans = max(ans, num + mp[sumDigits]);
+    
+                mp[sumDigits] = max(mp[sumDigits], num);
+            }
+    
+            return ans;
+        }
+    };
