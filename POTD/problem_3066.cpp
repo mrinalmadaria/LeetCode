e440e544ce746877174e920ad9d9644930cca360
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-02-14 00:05:36 +0530

    February 13 POTD

--- POTD/problem_3066.cpp
+++ POTD/problem_3066.cpp
@@ -1,2 +1,19 @@
 #include<bits/stdc++.h>
-using namespace std
+using namespace std;
+
+class Solution {
+    public:
+        int minOperations(vector<int>& a, int k) {
+            priority_queue<int, vector<int>, greater<int>> q(a.begin(), a.end());
+            for (int i = 0, x, y; ; ++i) {
+                x = q.top();
+                q.pop();
+                if (x >= k) return i;
+                y = q.top();
+                q.pop();
+                const long t = 2L * x + y; // test overflow
+                q.push(t < k ? (int)t : k);
+            }
+            return -1;
+        }
+    };
