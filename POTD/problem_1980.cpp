a4635dd8262b6af95cd14b33364a23f1a9d84847
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-02-20 23:51:39 +0530

    February 20 POTD

---
+++ POTD/problem_1980.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        string findDifferentBinaryString(vector<string>& nums) {
+            string result;
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i][i] == '0') {
+                    result += '1';
+                } else {
+                    result += '0';
+                }
+            }
+            return result;
+        }
+    };
