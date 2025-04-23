40988679583fb39dcdfd7e3282ce712ff6768e96
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-04-23 23:42:48 +0530

    April 23 POTD

---
+++ POTD/problem_1399.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int countLargestGroup(int n) {
+            std::unordered_map<int, int> map;
+            int maxSize = 0;
+            
+            for (int i = 1; i <= n; ++i) {
+                int sum = digitSum(i);
+                map[sum]++;
+                maxSize = std::max(maxSize, map[sum]);
+            }
+            
+            int count = 0;
+            for (const auto& pair : map) {
+                if (pair.second == maxSize) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        
+    private:
+        int digitSum(int num) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    };
