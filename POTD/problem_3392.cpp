786e120dbd13293a3c45a0c4454d7f9783a19d23
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-04-27 23:40:25 +0530

    April 27 POTD

---
+++ POTD/problem_3392.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums, int count = 0) {
+            for (int i = 0 ; i + 2 < nums.size(); i++)
+                if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) 
+                    count++;
+            return count;
+        }
+    };
