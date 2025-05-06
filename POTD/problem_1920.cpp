7ec61da0f37592a8f19d0ac8b57dc3e8b5a75810
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-05-06 23:53:30 +0530

    May 6 POTD

---
+++ POTD/problem_1920.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            for (int i = 0; i < nums.size(); i++) 
+                nums[i] += (1024 * (nums[nums[i]] % 1024));
+            
+            for (int i = 0; i < nums.size(); i++) 
+                nums[i] /= 1024;
+            
+            return nums;
+        }
+    };
