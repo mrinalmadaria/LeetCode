1b95cdf4c204f670c50966e6097c4e3ed1c52571
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-05-19 23:57:59 +0530

    May 19 POTD

---
+++ POTD/problem_3024.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        set<int>s(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
+        if((nums[0] + nums[1])<=nums[2]) return "none";
+        if(s.size() == 1) return "equilateral";
+        if(s.size() == 2) return "isosceles";
+        return "scalene";
+    }
+};
