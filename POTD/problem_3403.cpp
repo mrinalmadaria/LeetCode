c427d337399954a572e81010f5d22aa85367a774
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-06-04 23:40:09 +0530

    June 4 POTD

---
+++ POTD/problem_3403.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if(numFriends == 1) return word;
+        string res = "";
+
+        for(int i = 0; i < word.size(); i++){
+            res = max(res, word.substr(i, word.length() - numFriends + 1));
+        }
+
+        return res;
+    }
+};
