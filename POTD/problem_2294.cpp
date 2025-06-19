c4c8ca25da03997dd581f676485e360c40f6ca93
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-06-19 23:06:32 +0530

    June 19 POTD

---
+++ POTD/problem_2294.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int partitionArray(std::vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int ans = 1;
+        int min_val = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] - min_val > k) {
+                ans++;
+                min_val = nums[i];
+            }
+        }
+        return ans;
+    }
+};
