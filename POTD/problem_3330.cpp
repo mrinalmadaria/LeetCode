3edd3b0364437e26670feb54d129be3dd068b372
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-07-01 22:40:45 +0530

    July 1 POTD

---
+++ POTD/problem_3330.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n = word.length();
+        int count = n;
+        for (int i = 1; i < n; i++) {
+            if (word[i] != word[i - 1]) {
+                count--;
+            }
+        }
+        return count;
+    }
+};
