8269c0a6b99da390d012883c9fc4ad2c5169ffa1
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-07-22 22:36:22 +0530

    July 22 POTD

---
+++ POTD/problem_1695.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> seen;
+        int left = 0;
+        int currentSum = 0;
+        int maxSum = 0;
+        
+        for (int right = 0; right < n; ++right) {
+            while (seen.find(nums[right]) != seen.end()) {
+                currentSum -= nums[left];
+                seen.erase(nums[left]);
+                ++left;
+            }
+            currentSum += nums[right];
+            seen.insert(nums[right]);
+            maxSum = max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+};
